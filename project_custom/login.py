4a0227fbd7a1e133e6efd299bf4722de850aa7f9
Nave Industries <industriesnave@gmail.com>
2026-07-26 21:41:15 +0000

    Set Nave Home as login home page

--- project_custom/login.py
+++ project_custom/login.py
@@ -2,11 +2,5 @@ import frappe
 
 
 def redirect_to_nave_home(login_manager):
-    user = login_manager.user
-
-    if user and user != "Guest":
-        frappe.cache.hset(
-            "redirect_after_login",
-            user,
-            "/desk/nave-home",
-        )
+    if login_manager.user and login_manager.user != "Guest":
+        frappe.local.flags.home_page = "/desk/nave-home"
